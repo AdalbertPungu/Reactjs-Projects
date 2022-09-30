4a25f81c98d9a2db60ef9b09ca255cdfb31ca72b
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-09-30 22:06:15 +0200

    Add AppProvider components

--- submenu/src/context.js
+++ submenu/src/context.js
@@ -1,2 +1,47 @@
 import React, { useState, useContext } from 'react'
 import sublinks from './data'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+	const [isSubmenuOpen, setIsSubmenuOpen] = useState(false);
+	const [location, setLocation] = useState({});
+	const [page, setPage] = useState({ page: '', links: [] });
+
+	const openSidebar = () => {
+		setIsSidebarOpen(true);
+	};
+	
+	const closeSidebar = () => {
+		setIsSidebarOpen(false);
+	};
+
+	const openSubmenu = (text, coordinates) => {
+		const page = sublinks.find((link) => link.page === text);
+		setPage(page);
+		setLocation(coordinates);
+		setIsSubmenuOpen(true);
+	};
+	
+	const closeSubmenu = () => {
+		setIsSubmenuOpen(false);
+	};
+
+	return (
+		<AppContext.Provider
+			value={{
+				isSidebarOpen,
+				isSubmenuOpen,
+				openSidebar,
+				closeSidebar,
+				openSubmenu,
+				closeSubmenu,
+				location,
+				page,
+			}}
+		>
+			{children}
+		</AppContext.Provider>
+	);
+}
