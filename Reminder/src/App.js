243f57661c01da4aeddb6b0a4135e8a556f4c2a2
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-09-30 13:22:05 +0200

    Update App component

--- Reminder/src/App.js
+++ Reminder/src/App.js
@@ -1,8 +1,18 @@
 import React, { useState } from 'react';
 import data from './data';
 import List from './List';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+	const [people, setPeople] = useState(data); // data est un tableau d'objets
+  return (
+		<main>
+			<section className='container'>
+				<h3>{people.length} birthdays today</h3> // people.length est le nombre d'objets dans le tableau
+				<List people={people} /> // on passe le tableau d'objets à List
+				<button onClick={() => setPeople([])}>clear all</button> // on vide le tableau d'objets
+			</section>
+		</main>
+	);
 }
 
 export default App;
