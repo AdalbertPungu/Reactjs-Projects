0cc057df20b438357a7b3f098940adf70632bb2a
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-01 01:08:52 +0200

    Add useEffect in Submenu

--- submenu/src/Submenu.js
+++ submenu/src/Submenu.js
@@ -5,6 +5,22 @@ const Submenu = () => {
 	const { isSubmenuOpen, location, page: { page, links } } = useGlobalContext()
 	const container = useRef(null)
 	const [columns, setColumns] = useState('col-2')
+
+	useEffect(() => {
+		setColumns('col-2')
+		const submenu = container.current
+		const { center, bottom } = location
+		submenu.style.left = `${center}px`
+		submenu.style.top = `${bottom}px`
+
+		if (links.length === 3) {
+			setColumns('col-3')
+		}
+		if (links.length > 3) {
+			setColumns('col-4')
+		}
+	}, [location, links])
+
 }
 
 export default Submenu
