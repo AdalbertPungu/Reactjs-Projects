e52762ff534b8916ffb3776281937d7bfdcd7455
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-01 01:15:59 +0200

    Update Submenu.js

--- submenu/src/Submenu.js
+++ submenu/src/Submenu.js
@@ -21,6 +21,25 @@ const Submenu = () => {
 		}
 	}, [location, links])
 
+	return (
+		<aside
+			className={`${isSubmenuOpen ? 'submenu show' : 'submenu'}`}
+			ref={container}
+		>
+			<h4>{page}</h4>
+			<div className={`submenu-center ${columns}`}>
+				{links.map((link, index) => {
+					const { label, icon, url } = link
+					return (
+						<a key={index} href={url}>
+							{icon}
+							{label}
+						</a>
+					)
+				})}
+			</div>
+		</aside>
+	)
 }
 
 export default Submenu
