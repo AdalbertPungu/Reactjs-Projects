f760845ed5d532d85947a62d406519e223b067b0
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-01 13:04:39 +0200

    Update cocktail

--- cocktails/src/components/Cocktail.js
+++ cocktails/src/components/Cocktail.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-const Cocktail = () => {
-  return (
-    <div>
-      <h2>cocktail component</h2>
-    </div>
-  )
+const Cocktail = ({ image, name, id, info, glass }) => {
+	return (
+		<article className='cocktail'>
+			<div className='img-container'>
+				<img src={image} alt={name} />
+			</div>
+			<div className='cocktail-footer'>
+				<h3>{name}</h3>
+				<h4>{glass}</h4>
+				<p>{info}</p>
+				<Link to={`/cocktail/${id}`} className='btn btn-primary btn-details'>
+					details
+				</Link>
+			</div>
+		</article>
+	)
 }
 
 export default Cocktail
