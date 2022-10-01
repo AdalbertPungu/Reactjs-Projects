a654d7c6d6f2366cb27b68187e9e24989f03e818
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-01 10:21:56 +0200

    Update About pages

--- cocktails/src/pages/About.js
+++ cocktails/src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+				quibusdam, voluptatum, quae, voluptas quod voluptates quidem
+				accusantium voluptate quia natus quos. Quisquam, quae. Quisquam
+				quibusdam, voluptatum, quae, voluptas quod voluptates quidem
+				accusantium voluptate quia natus quos. Quisquam, quae.
+			</p>
+		</section>
   )
 }
 
