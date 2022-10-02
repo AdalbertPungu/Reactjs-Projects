86e73633b61d6f0f9c506542d1978bc3c83d46f6
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-02 21:25:03 +0200

    Update Banner.js

--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,12 +1,14 @@
-import '../styles/Banner.css'
 import logo from '../assets/logo.png'
+import '../styles/Banner.css'
 
 function Banner() {
-	return (
-		<div className='lmj-banner'>
-    	<h1>La maison jungle</h1>
-		</div>
-	)
+  const title = 'La maison jungle'
+  return (
+    <div className='lmj-banner'>
+      <img src={logo} alt='La maison jungle' className='lmj-logo' />
+        <h1 className='lmj-title'>{title}</h1>
+      </div>
+  )
 }
 
 export default Banner
