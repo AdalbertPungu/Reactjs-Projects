508c2a9e1112c7f6401bf5acab5278eb3e5cb033
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-03 03:11:14 +0200

    Import PlantItem

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import { plantList } from '../data/plantList'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
 
 function ShoppingList() {
@@ -16,11 +17,14 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-						{plant.name}
-						{plant.isSpecialOffer && <div className='lmj-sales'>Soldes</div>}
-					</li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem
+						key={id}
+						cover={cover}
+						name={name}
+						water={water}
+						light={light}
+					/>
 				))}
 			</ul>
 		</div>
