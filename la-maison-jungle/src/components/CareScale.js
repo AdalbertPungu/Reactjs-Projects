b77bbdd561dfe14b669418b5b0d0ed0d03a5b550
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-03 03:01:40 +0200

    Add quantityLabel

--- la-maison-jungle/src/components/CareScale.js
+++ la-maison-jungle/src/components/CareScale.js
@@ -1,6 +1,14 @@
 import Sun from '../assets/sun.svg'
 import Water from '../assets/water.svg'
 
+// Ici, il s'agit d'une manière de faire.
+//Vous auriez aussi pu utiliser une fonction qui retourne l'élément souhaité, ou bien faire directement des conditions
+const quantityLabel = {
+	1: 'peu',
+	2: 'modérément',
+	3: 'beaucoup'
+}
+
 function CareScale({ scaleValue, careType }) {
 	const range = [1, 2, 3]
 	const scaleType =
@@ -11,7 +19,15 @@ function CareScale({ scaleValue, careType }) {
 		)
 
 	return (
-		<div>
+		<div
+			onClick={() =>
+				alert(
+					`Cette plante requiert ${quantityLabel[scaleValue]} ${
+						careType === 'light' ? 'de lumière' : "d'arrosage"
+					}`
+				)
+			}
+		>
 			{range.map((rangeElem) =>
 				scaleValue >= rangeElem ? (
 					<span key={rangeElem.toString()}>{scaleType}</span>
