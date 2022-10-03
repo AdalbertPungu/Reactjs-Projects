80cd126c1c8635333a0a131390899379c2fa2383
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-03 15:31:55 +0200

    Update Banner component

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,14 +1,17 @@
 import Banner from './Banner'
 import logo from '../assets/logo.png'
-import Cart from './Cart'
+// import Cart from './Cart'
 import Footer from './Footer'
 import ShoppingList from './ShoppingList'
 
 function App() {
 	return (
 		<div>
-			<Banner />
-			<Cart />
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+			{/* <Cart /> */}
 			<ShoppingList />
 			<Footer />
 		</div>
