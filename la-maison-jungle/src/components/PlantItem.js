c39c6b2f15755d7f29fc395fed0fe6689bd96762
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-03 03:12:39 +0200

    Add handleClick event

--- la-maison-jungle/src/components/PlantItem.js
+++ la-maison-jungle/src/components/PlantItem.js
@@ -1,9 +1,13 @@
 import CareScale from './CareScale'
 import '../styles/PlantItem.css'
 
-function PlantItem({ id, cover, name, water, light }) {
+function handleClick(plantName) {
+	alert(`Vous voulez acheter 1 ${plantName}? Très bon choix 🌱✨`)
+}
+
+function PlantItem({ cover, name, water, light }) {
 	return (
-		<li key={id} className='lmj-plant-item'>
+		<li className='lmj-plant-item' onClick={() => handleClick}>
 			<img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
 			{name}
 			<div>
