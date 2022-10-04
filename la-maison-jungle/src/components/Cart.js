10d51991030846a3cd78ed430c8e88599d5a14d9
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-04 11:17:56 +0200

    Add updateCart

--- la-maison-jungle/src/components/Cart.js
+++ la-maison-jungle/src/components/Cart.js
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import '../styles/Cart.css'
 
-function Cart() {
-	const monsteraPrice = 8
-	const [cart, updateCart] = useState(0)
+function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
-
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price, 0
+	)
 	return isOpen ? (
 		<div className='lmj-cart'>
 			<button
@@ -14,11 +14,22 @@ function Cart() {
 			>
 				Fermer
 			</button>
-			<h2>Panier</h2>
-			<div>Monstera : {monsteraPrice} €</div>
-			<button onClick={() => updateCart(cart + 1)}>Ajouter</button>
-			<h3>Total : {monsteraPrice * cart} €</h3>
-			<button onClick={() => updateCart(0)}>Vider le panier</button>
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					<ul>
+						{cart.map(({ name, price, amount }, index) => (
+							<div key={`${name}-${index}`}>
+								{name} {price}€ x {amount}
+							</div>
+						))}
+					</ul>
+					<h3>Total :{total}€</h3>
+					<button onClick={() => updateCart([])}>Vider le panier</button>
+				</div>
+			) : (
+				<div>Votre panier est vide</div>
+			)}
 		</div>
 	) : (
 		<div className='lmj-cart-closed'>
