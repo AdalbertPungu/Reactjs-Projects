8f69951d8cffc2f798fcdd28f00f6fa523f3affb
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-04 17:33:21 +0200

    Add prices

--- la-maison-jungle/src/data/plantList.js
+++ la-maison-jungle/src/data/plantList.js
@@ -15,7 +15,8 @@ export const plantList = [
 		id: '1ed',
 		light: 2,
 		water: 3,
-		cover: monstera
+		cover: monstera,
+		price: 15
 	},
 	{
 		name: 'ficus lyrata',
@@ -23,7 +24,8 @@ export const plantList = [
 		id: '2ab',
 		light: 3,
 		water: 1,
-		cover: lyrata
+		cover: lyrata,
+		price: 16
 	},
 
 	{
@@ -32,7 +34,8 @@ export const plantList = [
 		id: '3sd',
 		light: 1,
 		water: 2,
-		cover: pothos
+		cover: pothos,
+		price: 9
 	},
 	{
 		name: 'calathea',
@@ -40,7 +43,9 @@ export const plantList = [
 		id: '4kk',
 		light: 2,
 		water: 3,
-		cover: calathea
+		cover: calathea,
+
+		price: 20
 	},
 	{
 		name: 'olivier',
@@ -48,7 +53,8 @@ export const plantList = [
 		id: '5pl',
 		light: 3,
 		water: 1,
-		cover: olivier
+		cover: olivier,
+		price: 25
 	},
 
 	{
@@ -57,7 +63,8 @@ export const plantList = [
 		id: '8fp',
 		light: 2,
 		water: 1,
-		cover: cactus
+		cover: cactus,
+		price: 6
 	},
 	{
 		name: 'basilique',
@@ -65,7 +72,8 @@ export const plantList = [
 		id: '7ie',
 		light: 2,
 		water: 3,
-		cover: basil
+		cover: basil,
+		price: 5
 	},
 	{
 		name: 'succulente',
@@ -73,7 +81,8 @@ export const plantList = [
 		id: '9vn',
 		light: 2,
 		water: 1,
-		cover: succulent
+		cover: succulent,
+		price: 8
 	},
 
 	{
@@ -82,6 +91,7 @@ export const plantList = [
 		id: '6uo',
 		light: 2,
 		water: 2,
-		cover: mint
+		cover: mint,
+		price: 4
 	}
 ]
