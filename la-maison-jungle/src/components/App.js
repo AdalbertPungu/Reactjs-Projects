70ab5ad104e3919e435bc0e836d0a2e0dda6181d
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-05 18:32:56 +0200

    Add useEffect

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Banner from './Banner'
 import logo from '../assets/logo.png'
 import Cart from './Cart'
@@ -7,11 +7,16 @@ import ShoppingList from './ShoppingList'
 import '../styles/Layout.css'
 
 function App() {
-	const [cart, updateCart] = useState([])
+	const savedCart = localStorage.getItem('cart')
+	const [cart, updateCart] = useState(savedCart ? JSON.parse(savedCart) : [])
+	useEffect(() => {
+		localStorage.setItem('cart', JSON.stringify(cart))
+	}, [cart])
+
 	return (
 		<div>
 			<Banner>
-				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<img src={logo} alt='logo-la-maison-jungle' className='lmj-logo' />
 				<h1 className='lmj-title'>La maison jungle</h1>
 			</Banner>
 			<div className='lmj-layout-inner'>
