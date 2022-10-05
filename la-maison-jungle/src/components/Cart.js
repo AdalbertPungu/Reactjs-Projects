80b964ef2fd24f51e40a7604f8bad6dc2872a8e2
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-05 17:07:01 +0200

    Add useEffect

--- la-maison-jungle/src/components/Cart.js
+++ la-maison-jungle/src/components/Cart.js
@@ -1,11 +1,16 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import '../styles/Cart.css'
 
 function Cart({ cart, updateCart }) {
 	const [isOpen, setIsOpen] = useState(true)
 	const total = cart.reduce(
-		(acc, plantType) => acc + plantType.amount * plantType.price, 0
+		(acc, plantType) => acc + plantType.amount * plantType.price,
+		0
 	)
+	useEffect(() => {
+		document.title = `LMJ: ${total}€ d'achats`
+	}, [total])
+
 	return isOpen ? (
 		<div className='lmj-cart'>
 			<button
