4f65e1de59ee8ec8274e08673686d604e7b9278a
AdalbertPungu <59202588+AdalbertPungu@users.noreply.github.com>
2022-10-07 02:39:42 +0200

    Update

---
+++ shiny-agence/src/pages/Home/index.jsx
@@ -0,0 +1,23 @@
+import logo from '../../logo.svg';
+import '../../styles/Home.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bonjour Adalbert</h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Home;
