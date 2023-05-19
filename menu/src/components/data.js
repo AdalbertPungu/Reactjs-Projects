8745ee1ef00edf2e3332c304ae39cae073abfd6b
Adalbert Pungu <59202588+AdalbertPungu@users.noreply.github.com>
2023-05-19 20:02:37 +0200

    Importing images links

--- menu/src/components/data.js
+++ menu/src/components/data.js
@@ -1,10 +1,21 @@
+import item1 from '../assets/item-1.jpeg'
+import item2 from '../assets/item-2.jpeg'
+import item3 from '../assets/item-3.jpeg'
+import item4 from '../assets/item-4.jpeg'
+import item5 from '../assets/item-5.jpeg'
+import item6 from '../assets/item-6.jpeg'
+import item7 from '../assets/item-7.jpeg'
+import item8 from '../assets/item-8.jpeg'
+import item9 from '../assets/item-9.jpeg'
+import item10 from '../assets/item-10.jpeg'
+
 const menu = [
   {
     id: 1,
     title: 'buttermilk pancakes',
     category: 'breakfast',
     price: 15.99,
-    img: '../assets/item-1.jpeg',
+    img: item1,
     desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
   },
   {
@@ -12,7 +23,7 @@ const menu = [
     title: 'diner double',
     category: 'lunch',
     price: 13.99,
-    img: '../assets/item-2.jpeg',
+    img: item2,
     desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
   },
   {
@@ -20,7 +31,7 @@ const menu = [
     title: 'godzilla milkshake',
     category: 'shakes',
     price: 6.99,
-    img: '../assets/item-3.jpeg',
+    img: item3,
     desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
   },
   {
@@ -28,7 +39,7 @@ const menu = [
     title: 'country delight',
     category: 'breakfast',
     price: 20.99,
-    img: '../assets/item-4.jpeg',
+    img: item4,
     desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
   },
   {
@@ -36,7 +47,7 @@ const menu = [
     title: 'egg attack',
     category: 'lunch',
     price: 22.99,
-    img: '../assets/item-5.jpeg',
+    img: item5,
     desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
   },
   {
@@ -44,7 +55,7 @@ const menu = [
     title: 'oreo dream',
     category: 'shakes',
     price: 18.99,
-    img: '../assets/item-6.jpeg',
+    img: item6,
     desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
   },
   {
@@ -52,7 +63,7 @@ const menu = [
     title: 'bacon overflow',
     category: 'breakfast',
     price: 8.99,
-    img: '../assets/item-7.jpeg',
+    img: item7,
     desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
   },
   {
@@ -60,7 +71,7 @@ const menu = [
     title: 'american classic',
     category: 'lunch',
     price: 12.99,
-    img: '../assets/item-8.jpeg',
+    img: item8,
     desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
   },
   {
@@ -68,7 +79,7 @@ const menu = [
     title: 'quarantine buddy',
     category: 'shakes',
     price: 16.99,
-    img: '../assets/item-9.jpeg',
+    img: item10,
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
 ];
