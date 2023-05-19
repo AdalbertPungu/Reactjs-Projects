4c7a4a1bab38689282369c1b13e8dfc0b647959b
Adalbert Pungu <59202588+AdalbertPungu@users.noreply.github.com>
2023-05-19 20:08:53 +0200

    Importing Categories Component

--- menu/src/App.js
+++ menu/src/App.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react'
-import items from './components/data'
-import { Menu } from './components/Menu'
+import React, { useState } from 'react';
+import items from './components/data';
+import { Menu } from './components/Menu';
+import Categories from './components/Categories';
+
+const allCategories = ['all', ...new Set(items.map((item) => item.category))];
 
 const App = () => {
-	const [menuItems, setMenuItems] = useState(items);
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
 
-	return (
-		<main>
+  return (
+    <main>
       <section className="menu section">
         <div className="title">
           <h2>our menu</h2>
           <div className="underline"></div>
         </div>
+        <Categories categories={categories} />
+        <Menu items={menuItems} />
       </section>
-			<Menu items={menuItems} />
     </main>
-	)
+  );
 }
 
 export default App;
